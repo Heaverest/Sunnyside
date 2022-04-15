9ea3b8a601f2b468f68da95e39996388a7f25ac6
Heaverest <djamzzy@gmail.com>
2022-04-15 04:10:03 -0700

    My first commit

--- sunnyside-agency-landing-page-main/index.js
+++
@@ -1,6 +0,0 @@
-let hamburgerMenu= document.querySelector(".ham");
-let navBar=document.querySelector(".nav-btn");
-
-hamburgerMenu.addEventListener("click", () =>{
-   navBar.classList.toggle("show"); 
-})
